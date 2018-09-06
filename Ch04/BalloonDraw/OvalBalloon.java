93c05f181471869c3f8c1b9b7f679969a262db12
Bshanks28 <bshankle28@gmail.com>
2018-09-06 09:18:01 -0500

    We created stub classes for FancyBalloon, SquareBalloon, and OvalBalloon to make the BalloonDraw.java run and make at least the round balloon and oval balloon using the correct draw method.

---
+++ Ch04/BalloonDraw/OvalBalloon.java
@@ -0,0 +1,31 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class OvalBalloon extends Balloon
+{
+   public OvalBalloon()
+   {
+      super();
+   }
+   
+   public OvalBalloon(int x, int y, int r, Color c)
+   {
+      super(x,y,r,c);
+   }
+   
+   public void draw(Graphics g, boolean makeItFilled)
+   {
+      int xCenter = getX();
+      int yCenter = getY();
+      int radius = getRadius();
+      Color color = getColor();
+      g.setColor(color);
+      if (makeItFilled)
+         g.fillOval(xCenter - radius,
+                    yCenter - radius, 2*radius, 4*radius);
+      else
+         g.drawOval(xCenter - radius,
+                    yCenter - radius, 2*radius, 4*radius);
+   }
+
+}
---
+++ Ch04/BalloonDraw/SquareBalloon.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class SquareBalloon extends Balloon
+{
+   public SquareBalloon()
+   {
+      super();
+   }
+   
+   public SquareBalloon(int x, int y, int r, Color c)
+   {
+      super(x,y,r,c);
+   }
+}
