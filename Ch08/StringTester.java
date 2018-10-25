683e70356ef69281a4c1c4c387dbd895a02c617b
Bshanks28 <bshankle28@gmail.com>
2018-10-25 08:58:57 -0500

    Tested how to work with strings.

---
+++ Ch08/StringTester.java
@@ -0,0 +1,15 @@
+public class StringTester
+{
+   
+   
+   
+   
+   public static void main(String[] args)
+   {
+      String example = "Window";
+      System.out.println(example.length());
+      
+      String letter = "" + example.charAt(4);
+      System.out.println(letter);
+   }
+}
