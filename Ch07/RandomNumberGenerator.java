d8e59c116cf56a54ac93b2f71928b2406abfc83d
Bshanks28 <bshankle28@gmail.com>
2018-10-30 08:32:17 -0500

    Added comments to program.

--- Ch07/RandomNumberGenerator.java
+++ Ch07/RandomNumberGenerator.java
@@ -1,15 +1,15 @@
-import java.util.Random;
+import java.util.Random; //imports random in place of using Math.rand()
 
 
-public class RandomNumberGenerator
+public class RandomNumberGenerator  //created the class
 {
    public static void main (String[] args)
    {
-      int max = 100;
-      int min = 1;
-      Random rand = new Random();
+      int max = 100;     //declares max value as an int
+      int min = 1;       // declare min value as an int
+      Random rand = new Random();    //creates the new random
       
-      int n = rand.nextInt(100) + 1;
-      System.out.println(n);
+      int n = rand.nextInt(100) + 1;    //creates the random number from 1-100
+      System.out.println(n);            // prints out random number made
    }
 }
