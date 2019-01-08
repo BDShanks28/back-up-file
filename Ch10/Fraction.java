291ce3f356bfe54c4b29f7515b83a8531c8e1402
Bshanks28 <bshankle28@gmail.com>
2019-01-08 09:16:50 -0600

    Made fractions in Fraction class.

--- Ch10/Fraction.java
+++ Ch10/Fraction.java
@@ -8,4 +8,16 @@ public class Fraction
       num = 0;
       denum = 1;
    }
+   
+   public Fraction(int num)
+   {
+      this.num = num;
+      this.denum = 1;
+   }
+   
+   public Fraction(int num, int denum)
+   {
+      this.num = num;
+      this.denum = denum;
+   }
 }
