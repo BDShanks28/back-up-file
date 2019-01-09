624d1afb6c8ea70cf2b5c961daf81edd3eb9ed59
Bshanks28 <bshankle28@gmail.com>
2019-01-09 09:18:18 -0600

    Finished the fraction runner and fraction class.

--- Ch10/Fraction.java
+++ Ch10/Fraction.java
@@ -5,19 +5,34 @@ public class Fraction
    
    public Fraction()
    {
-      num = 0;
-      denum = 1;
+      //num = 0;
+      //denum = 1;
+      this(0,1);
    }
    
    public Fraction(int num)
    {
-      this.num = num;
-      this.denum = 1;
+      //this.num = num;
+      //this.denum = 1;
+      this(num,1);
    }
    
    public Fraction(int num, int denum)
    {
       this.num = num;
       this.denum = denum;
+   }
+   
+   public Fraction(Fraction other)
+   {
+      //this.num = other.num;
+      //this.denum = other.denum;
+      this(other.num,other.denum);
+   }
+   
+   @Override
+   public String toString()
+   {
+      return this.num + "/" + denum;
    }
 }
---
+++ Ch10/FractionRunner.java
@@ -0,0 +1,20 @@
+public class FractionRunner
+{
+   public static void main(String[] args)
+   {
+      Fraction a = new Fraction(1,2);
+      System.out.println(a);
+      
+      Fraction b = new Fraction(5);
+      System.out.println(b);
+      
+      Fraction c = new Fraction();
+      System.out.println(c);
+      
+      Fraction d = new Fraction(b);
+      System.out.println(d);
+      
+      Fraction e = b;
+      System.out.print(e);
+   }
+}
