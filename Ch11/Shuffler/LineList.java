6f630949efef83f31d9c48911c3d470fda911c87
Bshanks28 <bshankle28@gmail.com>
2019-01-29 09:18:45 -0600

    Started working on shuffle project.

---
+++ Ch11/Shuffler/LineList.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class LineList
+{
+   //field
+   private ArrayList<String> lines;
+   
+   //constructor
+   
+   public LineList()
+   {
+      
+   }
+   //set n to the size of list
+   //randomly select an element among the first n
+   //decrement n by one
+   //repeat steps 2-3 while in >= 2
+   public void shuffle()
+   {
+      
+   }
+   
+   //methods
+   public int size()
+   {
+      return lines.size;
+   }
+   
+   public String get(int k)
+   {
+      return lines.get;
+   }
+   
+   public void add(string)
+   {
+      list.add(line);
+   }
+   
+   public String remove(int k)
+   {
+      list.remove(k);
+   }
+   
+   
+   public void move(int index, int newIndex)
+   {
+      
+   }
+}
