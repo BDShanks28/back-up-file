5bd20c8be294c16e49d4e61b43895ad017c7a40d
Bshanks28 <bshankle28@gmail.com>
2019-01-30 09:15:29 -0600

    Finished LineList.java.

--- Ch11/Shuffler/LineList.java
+++ Ch11/Shuffler/LineList.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class LineList
 {
    //field
    private ArrayList<String> lines;
+   private Random rand;
    
    //constructor
    
    public LineList()
    {
-      
+      lines = new ArrayList<String>();
+      Random rand = new Random();
    }
    //set n to the size of list
    //randomly select an element among the first n
@@ -17,33 +20,43 @@ public class LineList
    //repeat steps 2-3 while in >= 2
    public void shuffle()
    {
-      
+      int n = lines.size();
+      while(n >= 2)
+      {
+         int r = rand.nextInt(n-1);
+         String temp = lines.get(n-1);
+         lines.set(n-1, lines.get(r));
+         lines.set(r, temp);
+         n--;
+      }
    }
    
    //methods
    public int size()
    {
-      return lines.size;
+      return lines.size();
    }
    
    public String get(int k)
    {
-      return lines.get;
+      return lines.get(k);
    }
    
-   public void add(string)
+   public void add(String line)
    {
-      list.add(line);
+      lines.add(line);
    }
    
    public String remove(int k)
    {
-      list.remove(k);
+      return lines.set(k, "");
    }
    
    
    public void move(int index, int newIndex)
    {
-      
+      String temp = lines.get(index);
+      lines.set(index, lines.get(newIndex));
+      lines.set(newIndex, temp);
    }
 }
