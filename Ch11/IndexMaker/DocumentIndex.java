50c41b8fa0378125e4517755c128dbc559f93e0b
Bshanks28 <bshankle28@gmail.com>
2019-02-05 09:08:49 -0600

    Worked on DocumentIndex some

--- Ch11/IndexMaker/DocumentIndex.java
+++ Ch11/IndexMaker/DocumentIndex.java
@@ -1,21 +1,48 @@
 import java.util.ArrayList;
 
-public class DocumentIndex
+public class DocumentIndex extends ArrayList<IndexEntry>
 {
-   private int foundOrInserted(String word)
+
+   /**
+   * adds num to the IndexEntry for word by calling that
+   * IndexEntry's add(num) method. if word is not yet in this
+   * DocumentIndex, make a new IndexEntry for word
+   * @param wor Word to be added
+   * @param num current line number
+   */      
+   public void addWord(String word, int num)
    {
-      for(
+      for(int i = 0; i < this.size(); i++)
+      {
+         if(word.compareTo(this.get(i).getWord()) == 0)
+         {
+            
+            
+            return;
+         }
+         else if(word.conmpareTo(this.get(i).getWord()) > 0)
+         {
+            
+            
+            
+            return;
+         }
+      }
    }
    
-   public void addAllWords(String word, int line)
+   public void addAllWords(String word, int num)
+   {
+      String[] split = word.split("\\W+");
+      for(int i = 0; i < split.length; i++)
+      {
+         
+      }
+   }
+
+   
+   private int foundOrInserted(int num)
    {
       
    }
-   
-   public void addWord(String word, int line)
-   {
-      
-   }
-   
-   
+
 }
