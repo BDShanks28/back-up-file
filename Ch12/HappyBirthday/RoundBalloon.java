25af77407f2282c12a68d37424f7e16570695d0a
BDShanks28 <bshankle28@gmail.com>
2019-02-25 15:15:16 +0000

    Worked on Balloon.java.

---
+++ Ch12/HappyBirthday/RoundBalloon.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class RoundBalloon extends Balloon
+{
+	public RoundBalloon()
+	{
+		super();
+	}
+
+// Creates a RoundBalloon with given properties
+//@param x x-position
+//@param y y-position
+//@param r radius
+//@param c Color
+	public RoundBalloon(int x, int y, int r, Color c)
+	{
+		super(x, y, r, c);
+	}
+}
