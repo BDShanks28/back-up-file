22766a2ad4558704e909ef5c167a685c42f26e41
BDShanks28 <bshankle28@gmail.com>
2019-03-04 15:14:44 +0000

    Worked on Triangle activity.

--- Ch12/Exercises/Triangle.java
+++ Ch12/Exercises/Triangle.java
@@ -2,45 +2,20 @@
 
 public class Triangle
 {
-  private String type;
-  private double side;
+	private String type;
+	private double side;
 
-  public Triangle(String type, double side)
-  {
-    this.type = type;
-    this.side = side;
-  }
+	public Triangle(, double side)
+  	{
+    		this.side = side;
+  	}
 
-  public double getPerimeter()
-  {
-    if ("equilateral".equals(type))
-      return 3 * side;
-    else if ("right".equals(type))
-      return (2 + Math.sqrt(2.0)) * side;
-    else
-      throw new IllegalArgumentException("Invalid triangle type");
-  }
+  	public double getPerimeter();
 
-  public double getArea()
-  {
-    if ("equilateral".equals(type))
-      return Math.sqrt(3) / 4 * side * side;
-    else if ("right".equals(type))
-      return side * side / 2;
-    else
-      throw new IllegalArgumentException("Invalid triangle type");
-  }
+  	public abstract double getArea();
 
-  public double getRatio()
-  {
-    return getArea()/getPerimeter();
-  }
-
-  public static void main(String[] args)
-  {
-    Triangle equilateralTr = new Triangle("equilateral", 12);
-    Triangle rightTr = new Triangle("right", 12);
-    System.out.println("Equilateral " + equilateralTr.getRatio());
-    System.out.println("Right isosceles " + rightTr.getRatio());
-  }
+  	public double getRatio()
+  	{
+    		return getArea()/getPerimeter();
+  	}
 }
---
+++ Ch12/Exercises/TriangleRunner.java
@@ -0,0 +1,7 @@
+public class TriangleRunner
+{
+	public static void main(String[] args)
+        {
+                
+        }
+}
