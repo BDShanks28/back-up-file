2959aa1c611af972a3a6555565187fb5cf4120f2
BDShanks28 <bshankle28@gmail.com>
2019-03-07 21:07:20 +0000

    Completed the TriangleRunner (I believe I did at least).

--- Ch12/Exercises/TriangleRunner.java
+++ Ch12/Exercises/TriangleRunner.java
@@ -3,7 +3,7 @@ public class TriangleRunner
 	public static void main(String[] args)
         {
                 Triangle[] myTriangle = new Triangle [2];
-		myTriangle[0] = new RightTriangle(double side);
-		myTriangle[1] = new EquilateralTriangle(double side);
+		myTriangle[0] = new RightTriangle(5);
+		myTriangle[1] = new EquilateralTriangle(5);
         }
 }
